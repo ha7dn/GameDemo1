9c7678397a54e3cbb6e343bb3351729fdff80209
Ayekaba <ayekaba.mengue@hotmail.com>
2020-05-24 18:44:00 +0200

    Game Demo 1

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    // Config parameters
+
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 10f;
+
+    // State 
+    Vector2 paddleToBall;
+    bool hasGameStarted = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBall = transform.position - paddle1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!hasGameStarted)
+            LockBallToPaddle();
+
+        LaunchOnClick();
+    }
+
+    private void LaunchOnClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+            hasGameStarted = true;
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+
+        var paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddleToBall + paddlePos;
+
+
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    // Orthographic Camera Size = 1/2 height
+    // 108px per Unit
+
+    // Config parameters 
+    float totalWidth = 35.55f;
+    float maxX = 33.05f;
+    float paddleWidth = 2.5f;
+
+    private void Update()
+    {
+        var mousePos = Input.mousePosition.x / Screen.width * totalWidth;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePos, paddleWidth - totalWidth, maxX);
+        transform.position = paddlePos;
+    }
+
+
+}
